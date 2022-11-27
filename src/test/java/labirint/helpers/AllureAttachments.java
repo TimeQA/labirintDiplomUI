3f681c06302ba18f7e0158cc50bd3d5fb200a971
TimeQA <qaeducation2022@gmail.com>
2022-11-27 11:42:06 +0300

    all project

---
+++ src/test/java/labirint/helpers/AllureAttachments.java
@@ -0,0 +1,55 @@
+package labirint.helpers;
+
+import io.qameta.allure.Attachment;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
+
+public class AllureAttachments {
+    public static final Logger LOGGER = LoggerFactory.getLogger(AllureAttachments.class);
+
+    @Attachment(value = "{attachName}", type = "text/plain")
+    private static String addMessage(final String attachName, final String text) {
+        return text;
+    }
+
+    public static void addBrowserConsoleLogs() {
+        addMessage("Browser console logs", DriverUtils.getConsoleLogs());
+    }
+
+    @Attachment(value = "{attachName}", type = "image/png")
+    public static byte[] addScreenshotAs(final String attachName) {
+        return DriverUtils.getScreenshotAsBytes();
+    }
+
+    @Attachment(value = "Page source", type = "text/html")
+    public static byte[] addPageSource() {
+        return DriverUtils.getPageSourceAsBytes();
+    }
+
+    @Attachment(value = "Video", type = "text/html", fileExtension = ".html")
+    public static String addVideo() {
+        return "<html><body><video width='100%' height='100%' controls autoplay><source src='"
+                + getVideoUrl()
+                + "' type='video/mp4'></video></body></html>";
+    }
+
+    public static URL getVideoUrl() {
+        String videoUrl = "https://selenoid.autotests.cloud/video/" + getSessionId() + ".mp4";
+
+        try {
+            return new URL(videoUrl);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+    public static String getSessionId(){
+        return ((RemoteWebDriver) getWebDriver()).getSessionId().toString();
+    }
+}
---
+++ src/test/java/labirint/tests/ui/pages/MainPage.java
@@ -0,0 +1,132 @@
+package labirint.tests.ui.pages;
+
+
+import com.codeborne.selenide.CollectionCondition;
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.ElementsCollection;
+import com.codeborne.selenide.SelenideElement;
+
+import java.util.List;
+
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$$;
+
+public class MainPage {
+
+
+    private final SelenideElement searchField = $("#search-field");
+
+    private final SelenideElement basketLink = $(".cart-icon-js");
+
+    private final SelenideElement favoritesLink = $(".b-header-b-personal-e-icon-count-m-putorder");
+
+    //    top-link-main_putorder
+    private final SelenideElement headerMenu = $(".b-header-b-menu-wrapper");
+
+    private final SelenideElement addBasketSomeProduct = $(".product_labeled:nth-child(1)");
+
+    private final SelenideElement selectProductOnMainPage = $(".product-buy-margin:nth-child(1)");
+
+    private final SelenideElement formaliseButtonSomeProduct = $(".product-buy-margin:nth-child(1)");
+
+    private final SelenideElement firstProductInFavorites = $(".icon-fave:nth-child(1)");
+    private final SelenideElement selectedProductInBasketOrFavorites = $(".need-watch");
+
+//    private final SelenideElement quantityInBasket = $("li.ui-corner-top:nth-child(1)")
+//            .shouldBe(Condition.text("1"));
+//
+//    private final SelenideElement quantityInFavourites  = $("li.ui-corner-top:nth-child(1)")
+//            .shouldBe(Condition.text("1"));
+
+    private final ElementsCollection itemDropDownMenu = $$("ul li.b-menu-second-item");
+
+    //    Search
+    private final ElementsCollection elementsOnPage = $$("div .genres-carousel__item");
+
+//    Basket
+
+    private final SelenideElement buttonAddBasket = $(".buy-link");
+
+    private final SelenideElement myBasketTitle = $(".basket-page__title");
+
+//    public final MainPage openPage() {
+//        open(baseUrl);
+//
+//        return this;
+//    }
+
+    public final MainPage healthCheck() {
+        elementsOnPage.shouldBe(CollectionCondition.sizeGreaterThan(0));
+
+        return this;
+    }
+
+    public final MainPage clickButtonAddedProductBasket() {
+        selectedProductInBasketOrFavorites.$(byText("В КОРЗИНУ")).click();
+        return this;
+    }
+
+    public final MainPage selectProduct() {
+        selectProductOnMainPage.$(byText("В КОРЗИНУ")).click();
+        return this;
+    }
+
+    public final MainPage addFirstProductInFavorites() {
+        firstProductInFavorites.click();
+
+        return this;
+    }
+
+    public final MainPage clickButtonFormaliseOrder() {
+        formaliseButtonSomeProduct.shouldBe(Condition.text("ОФОРМИТЬ")).click();
+
+        return this;
+    }
+
+    public final MainPage searchBook(final String bookName) {
+        searchField.setValue(bookName).pressEnter();
+
+        return this;
+    }
+
+
+    public final MainPage openBasketPage() {
+        basketLink.click();
+
+        return this;
+    }
+
+
+    public final MainPage selectItemHeaderMenu(String typeDevice) {
+        headerMenu.$(byText(typeDevice)).hover();
+
+        return this;
+    }
+
+    public final MainPage checkSubitemDropDownMenu(List<String> expectedTypeDevice) {
+        itemDropDownMenu.contains(expectedTypeDevice);
+
+        return this;
+    }
+
+    public final MainPage goFavoritesPage() {
+        favoritesLink.click();
+
+        return this;
+    }
+
+    public final MainPage goBasketPage() {
+        basketLink.click();
+
+        return this;
+
+    }
+
+
+    public final MainPage checkProductOnBasketOrFavoritesPage(String bookName) {
+        selectedProductInBasketOrFavorites.shouldBe(Condition.text(bookName));
+
+        return this;
+    }
+}
