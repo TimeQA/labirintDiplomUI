a0fb46060b1bf272b566bf03e18dc84764642336
TimeQA <qaeducation2022@gmail.com>
2022-12-03 14:58:23 +0300

    delete some packege

--- src/test/java/labirint/helpers/AllureAttachments.java
+++ src/test/java/labirint/helpers/AllureAttachments.java
@@ -35,21 +35,23 @@ public class AllureAttachments {
     @Attachment(value = "Video", type = "text/html", fileExtension = ".html")
     public static String addVideo() {
         return "<html><body><video width='100%' height='100%' controls autoplay><source src='"
-                + getVideoUrl()
+                + DriverUtils.getVideoUrl(DriverUtils.getSessionId())
                 + "' type='video/mp4'></video></body></html>";
     }
 
-    public static URL getVideoUrl() {
-        String videoUrl = "https://selenoid.autotests.cloud/video/" + getSessionId() + ".mp4";
 
-        try {
-            return new URL(videoUrl);
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
-    public static String getSessionId(){
-        return ((RemoteWebDriver) getWebDriver()).getSessionId().toString();
-    }
+
+//    public static URL getVideoUrl() {
+//        String videoUrl = "https://selenoid.autotests.cloud/video/" + getSessionId() + ".mp4";
+//
+//        try {
+//            return new URL(videoUrl);
+//        } catch (MalformedURLException e) {
+//            e.printStackTrace();
+//        }
+//        return null;
+//    }
+//    public static String getSessionId(){
+//        return ((RemoteWebDriver) getWebDriver()).getSessionId().toString();
+//    }
 }
